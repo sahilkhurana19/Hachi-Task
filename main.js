3d17cb9833c24a06271d060d817f2ea6109709a3
sahilkhurana02 <sahil_khurana@outlook.com>
2017-05-08 01:51:11 +0530

    Added animation

---
+++ main.js
@@ -0,0 +1,10 @@
+$("document").ready(function() {
+    //$(".card-1").addClass("animated fadeIn")
+    //$(".card-2").delay(5000).addClass("animated fadeIn")
+    //$(".card-3").delay(10000).addClass("animated fadeIn")
+
+})
+    $(".card-1").hide().fadeIn(800);
+    $(".card-2").hide().fadeIn(1100);
+    $(".card-3").hide().fadeIn(1500);    
+
